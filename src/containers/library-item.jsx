82f8b651810ac290fb9fe51e405b67825c3dbec2
Aquino Luane <aquino@macareux.co.jp>
2024-10-15 11:24:06 +0900

    feat: add a sprite

--- src/containers/library-item.jsx
+++ src/containers/library-item.jsx
@@ -106,7 +106,7 @@ class LibraryItem extends React.PureComponent {
     render () {
         const iconMd5 = this.curIconMd5();
         const iconURL = iconMd5 ?
-            `https://cdn.assets.scratch.mit.edu/internalapi/asset/${iconMd5}/get/` :
+            `http://localhost:3000/image/${iconMd5}` :
             this.props.iconRawURL;
         return (
             <LibraryItemComponent
