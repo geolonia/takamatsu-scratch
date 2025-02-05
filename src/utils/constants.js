43fe80eb1d088d702cc88636f919dec42b2d264f
Aquino Luane <aquino@macareux.co.jp>
2025-02-05 10:40:03 +0900

    refactor(constants.js): remove unnecessary code

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,1 @@
-// export const BASE_API_URL_LOCAL = "http://localhost:3000"; // FIXME: Change this to the actual API URL
-// export const BASE_API_URL = "http://localhost:3000";
-// export const BASE_API_URL = "http://localhost:8301";
 export const BASE_API_URL = "https://chizubouken-lab.jp";
