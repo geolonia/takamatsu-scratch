9f215d798bd9640d0a0b1a667adb8949b578f3dc
Aquino Luane <aquino@macareux.co.jp>
2025-03-05 14:42:43 +0900

    refactor(constants): remove mock url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,3 @@
-// export const BASE_API_URL = "https://chizubouken-lab.jp";
-export const BASE_API_URL = "http://localhost:8301";
+export const BASE_API_URL = "https://chizubouken-lab.jp";
 export const TOKEN_KEY = "access_token";
 export const REFRESH_TOKEN_KEY = "refresh_token";
