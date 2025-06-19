f845edc57e4e50ac9d0c074342dacb6262c99fe5
mdnakazawa <n.yuumi@macareux.co.jp>
2025-06-20 07:06:47 +0900

    remove fixed url so that it can be used in develop site as well

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const BASE_API_URL = "https://chizubouken-lab.jp";
+export const BASE_API_URL = "";
 export const TOKEN_KEY = "access_token";
 export const REFRESH_TOKEN_KEY = "refresh_token";
